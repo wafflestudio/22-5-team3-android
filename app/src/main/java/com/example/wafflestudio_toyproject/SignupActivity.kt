d79adf9f34804b9f0fac7e2913d60e5750f66746
huiwooj <ashleyhuiwoo@gmail.com>
2025-01-03 01:04:20 +0900

    signup add realname department

--- app/src/main/java/com/example/wafflestudio_toyproject/SignupActivity.kt
+++ app/src/main/java/com/example/wafflestudio_toyproject/SignupActivity.kt
@@ -1,5 +1,7 @@
 package com.example.wafflestudio_toyproject
+import android.R
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wafflestudio_toyproject.databinding.ActivitySignupBinding
@@ -22,23 +24,37 @@ class SignupActivity : AppCompatActivity() {
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //학과 선택 스피너 세팅
+        val departments = listOf("인문대학", "사회과학대학", "자연과학대학", "간호대학", "경영대학", "공과대학",
+            "농업생명과학대학", "미술대학", "사범대학", "생활과학대학", "수의과대학", "약학대학", "음악대학", "의과대학",
+            "자유전공학부", "첨단융합학부")
+        val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, departments)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.departmentSpinner.adapter = adapter
+
         // signupButton 클릭 리스너
         binding.signupButton.setOnClickListener {
             val username = binding.usernameEditText.text.toString()
             val password = binding.passwordEditText.text.toString()
             val email = binding.emailEditText.text.toString()
+            val realname = binding.nameEditText.text.toString()
+            val department = binding.departmentSpinner.selectedItemPosition + 1
 
-            if (username.isNotEmpty() && password.isNotEmpty() && email.isNotEmpty()) {
-                signup(username, email, password)
+            if (username.isNotEmpty() && password.isNotEmpty() && email.isNotEmpty()  && realname.isNotEmpty()) {
+                signup(username, email, password, realname, department)
+            } else {
+                Toast.makeText(this, "모든 항목을 입력해주세요.", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun signup(username: String, email: String, password: String) {
+    private fun signup(username: String, email: String, password: String, realname: String, department: Int) {
         userRepository.signup(
             username,
             email,
             password,
+            realname,
+            department,
             onSuccess = { response ->
                 Toast.makeText(this, "회원가입 성공: ${response.email}", Toast.LENGTH_SHORT).show()
             },
